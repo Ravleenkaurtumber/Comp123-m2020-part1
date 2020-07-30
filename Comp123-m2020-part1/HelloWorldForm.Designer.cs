94fbe74fbbb913114c700d72db1095e1d468469c
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-30 15:01:38 -0400

    added access button

--- Comp123-m2020-part1/HelloWorldForm.Designer.cs
+++ Comp123-m2020-part1/HelloWorldForm.Designer.cs
@@ -84,6 +84,7 @@
             // 
             // HelloWorldForm
             // 
+            this.AcceptButton = this.ClickMeButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
