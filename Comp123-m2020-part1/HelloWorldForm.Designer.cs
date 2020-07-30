d7b063227d19458a734e195232f82adce706b34b
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2020-07-30 14:17:51 -0400

    added label and button

--- Comp123-m2020-part1/HelloWorldForm.Designer.cs
+++ Comp123-m2020-part1/HelloWorldForm.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.ClickMeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WelcomeLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.WelcomeLabel.Location = new System.Drawing.Point(231, 190);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(178, 47);
+            this.WelcomeLabel.TabIndex = 0;
+            this.WelcomeLabel.Text = "Hello, World!";
+            this.WelcomeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.WelcomeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ClickMeButton
+            // 
+            this.ClickMeButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClickMeButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.ClickMeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.ClickMeButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.ClickMeButton.Location = new System.Drawing.Point(241, 268);
+            this.ClickMeButton.Name = "ClickMeButton";
+            this.ClickMeButton.Size = new System.Drawing.Size(158, 43);
+            this.ClickMeButton.TabIndex = 1;
+            this.ClickMeButton.Text = "Click Me!";
+            this.ClickMeButton.UseVisualStyleBackColor = false;
+            this.ClickMeButton.Click += new System.EventHandler(this.ClickMeButton_Click);
+            // 
+            // HelloWorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.ClickMeButton);
+            this.Controls.Add(this.WelcomeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "HelloWorldForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World!";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label WelcomeLabel;
+        private System.Windows.Forms.Button ClickMeButton;
     }
 }
 
--- Comp123-m2020-part1/HelloWorldForm.cs
+++ Comp123-m2020-part1/HelloWorldForm.cs
@@ -16,5 +16,19 @@ namespace Comp123_m2020_part1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// this method changes the text property of welcome label on its intial value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            WelcomeLabel.Text = (WelcomeLabel.Text == "Hello, World!") ? "Clicked!" : "Hello, World!";
+        }
     }
 }
